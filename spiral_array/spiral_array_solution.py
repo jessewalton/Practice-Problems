2e4f88dd67ec7816e80da90d441cbf7f54e890ff
Jesse Walton <jesse.walton@email.com>
2016-03-12 16:44:02 -0500

    Start here! Problem structure finalized

--- spiral_array/spiral_array_solution.py
+++ spiral_array/spiral_array_solution.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from itertools import izip
+
 '''
 	Problem: 
 		Given a n*m array, start at the top left and traverse it in a clockwise
@@ -8,7 +10,17 @@
 '''
 
 
+
+
 ''' Functions '''
+
+# traverse array and return as a list
+def traverse_cw_spiral(ARRAY_NxM):
+	traverse_order = []
+
+	# define function 
+
+	return traverse_order
 
 # build a 2 dimensional array and fill with values 1 to NxM
 def build_2d_array(N, M):
@@ -31,17 +43,43 @@ def print_2d_array(ARRAY):
 		print 
 	print
 
+# check elements in two lists
+def compare_lists(CORRECT_LIST, PROPOSED_LIST):
+	needed_matches = len(CORRECT_LIST)
+	actual_matches = 0
+
+	for list_1_item, list_2_item in izip(CORRECT_LIST, PROPOSED_LIST):
+		if list_1_item == list_2_item:
+			actual_matches += 1
+	
+	if actual_matches == needed_matches:
+		return True
+
+# check the solution to a 4 by 4 array
+def check_4by4(CHECK_LIST_ORDER):
+	print "4 by 4:",
+	correct_order = [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 12, 10]
+	if compare_lists(correct_order, CHECK_LIST_ORDER):
+		print "Lists match"
+		return True
+	else: 
+		print "Lists do not match"
+		return False
+
+
 
 
 ''' Main ''' 
 
-# create and fill array
-array_2d = build_2d_array(4, 4)
+# create and fill 4 by 4 array
+array_4by4 = build_2d_array(4, 4)
 
+# display formatted array
+print_2d_array(array_4by4)
 
-# display array
-print_2d_array(array_2d)
+# traverse array
+traverse_order_4by4 = traverse_cw_spiral(array_4by4)
 
-
-
+# check traversal
+check_4by4(traverse_order_4by4)
 
