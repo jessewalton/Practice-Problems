e4832077c21d7aa9e2bc9b7efcbe4b91cd6c3f2b
Jesse Walton <jesse.walton@email.com>
2016-03-22 18:08:47 -0400

    Add vending_machine.py

---
+++ vending_machine/vending_machine.py
@@ -0,0 +1,32 @@
+#!/usr/bin/var python
+
+
+class Vending_Machine(object):
+  'Virtual vending machine to demonstrate object oriented design principles'
+  
+  def __init__(self):
+    self.input_amount = 0     # track cust. money deposited
+    self.coin_bank = 0        # amount of collected money
+    self.inventory = []       # hold inventory
+    
+  def insertMoney(self, amount):
+    self.coin_bank += amount 
+
+
+class Snack(object):
+  'Virtual base class for vending machine snacks' 
+  def __init__(self, name):
+    self.cost = 0.00            
+    self.name = name
+  pass
+
+
+class Soda(Snack):
+  def __init__(self, name):
+    Snack.__init__(self, name)
+    self.cost = 0.50
+
+
+class Chips(Snack):
+  def __init__(self):
+    self.cost = 0.75
